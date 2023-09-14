3355c0d50156fe20523b608f697a823670b0d221
Zahid_Khan <xahiidkhan@gmail.com>
2023-09-14 15:37:41 +0530

    First commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Calculator {
+    public void print() {
+        System.out.printf("hello");
+    }
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public int multiple(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+
+}
